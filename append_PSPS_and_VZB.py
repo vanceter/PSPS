d63c5dc62c0c7ac115c1a5702f3a25caf12b31cf
Terry <70032002+vanceter@users.noreply.github.com>
2022-02-14 13:30:47 -0800

    added notes

--- append_PSPS_and_VZB.py
+++ append_PSPS_and_VZB.py
@@ -4,12 +4,11 @@ import pandas as pd
 import xlsxwriter
 
 df = pd.read_excel("PSPS_MAIN.xlsx")
-df2 = pd.read_excel("2022_PSPS_VZB_Sites.xlsx")
+df2 = pd.read_excel("PSPS_VZB_Sites.xlsx")
+frames = [df, df2]
+df3 = pd.concat(frames)
+#print(df.concat)
+writer_vzb = pd.ExcelWriter('PSPS_MAIN_VZB.xlsx', engine='xlsxwriter')
+df3.to_excel(writer_vzb, index=False, sheet_name='PSPS_MAIN_VZB')
 
-df.append(df2, ignore_index=True)
-df.to_excel("PSPS_MAIN_VZB.xlsx", index = False, sheet_name='PSPS_MAIN_VZB')
-
-# df = pd.read_excel('tmp.xlsx', sheet_name=None, index_col=None)
-# cdf = pd.concat(df.values())
-
-#print(df)
+writer_vzb.save()
