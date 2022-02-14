a872ddb29c893764db1bfcf99ce0d1e894122a7e
Terry <70032002+vanceter@users.noreply.github.com>
2022-02-14 13:27:13 -0800

    Added the ability to concat the VZB location file to the PSPS main sheets, ops and SP, as well as cleaned out some unnecessary comments

--- merge_excel_sheets.py
+++ merge_excel_sheets.py
@@ -7,13 +7,6 @@ import xlsxwriter
 # NOTE each of the underlying xls files needs to have the PSLC value - in some of them, the column header needs to be renamed from PS Loc
 # Also need to make sure you export OpsTracker files with the file name option checked
  
-# reading the entire files - commented out to use the commands below instead, pulling only the columns needed
-# f_sites = pandas.read_excel("opstracker_sites.xlsx")
-# f_gens = pandas.read_excel("opstracker_generators.xlsx")
-# f_cells = pandas.read_excel("NorCal_CellInfo.xlsx")
-# f_cells5g = pandas.read_excel("norcal_cell_info_5g.xlsx")
-# f_pge = pandas.read_excel("PSPS_FIRE_TIER.xlsx")
-
 # reading only the columns needed from each file
 # documentation on pandas read_excel https://pandas.pydata.org/docs/reference/api/pandas.read_excel.html
 f_sites = pd.read_excel("opstracker_sites.xlsx", usecols=['SITE_NAME','ADDRESS','CITY','COUNTY','PSLC','POWER_METER', 'GEN_STATUS','GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'IS_HUB','IS_HUB_MICROWAVE','REMOTE_MONITORING','SITETECH_NAME','SITETECH_MANAGER_NAME', 'POWER_COMPANY'])
@@ -21,7 +14,8 @@ f_gens = pd.read_excel("opstracker_generators.xlsx", usecols=['PSLC', 'FUEL_TYPE
 f_cells = pd.read_excel("NorCal_CellInfo.xlsx", usecols=['PSLC', 'eNodeB'])
 f_cells5g = pd.read_excel("norcal_cell_info_5g.xlsx", usecols=['PSLC', 'GNODEB'])
 f_pge = pd.read_excel("PSPS_FIRE_TIER.xlsx", usecols=['PSLC', 'Fire Tier', 'PSPS PROB', 'PG&E Fee Property'])
-  
+f_vzb = pd.read_excel("PSPS_VZB_Sites.xlsx")
+
 # merging the files using PSLC as the index. There are some duplicates in gen and sites files, lots of duplicates in the cell info because of B2B and 5G gNodeBs
 # documentation on pandas merge https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.merge.html?highlight=merge#pandas.DataFrame.merge
 f_merged = f_sites.merge(f_gens, left_on="PSLC", right_on="PSLC", how="left")
@@ -32,18 +26,21 @@ f_merged = f_merged.merge(f_cells5g, left_on="PSLC", right_on="PSLC", how="left"
 # same function but only combining the sites, gens and PSPS/PGE files for Ops
 f_merged_ops = f_sites.merge(f_gens, left_on="PSLC", right_on="PSLC", how="left")
 f_merged_ops = f_merged_ops.merge(f_pge, left_on="PSLC", right_on="PSLC", how="left")
-  
-# creating 2 new files, the PSPS_Main for Gennie, and a version of it with eNB/gNB for SP
-# documentation on pandas to_excel https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_excel.html?highlight=to_excel
-#f_merged.to_excel("PSPS_MAIN_SP.xlsx", index = False, sheet_name='PSPS_MAIN_SP', columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME', 'POWER_COMPANY', 'eNodeB', 'GNODEB'])
-#f_merged_ops.to_excel("PSPS_MAIN.xlsx", index = False, sheet_name='PSPS_MAIN', columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME'])
-# Change the write tool to include ability to add formatting to the output 
+# create a new dataframe to concatenate the merged data with a static VZB/VZS file of meter numbers from PGE for non-wireless locations
+# requires that the PSPS_VZB_Sites.xlsx file exist in the directory, same format as PSPS_MAIN, but with the random meters PGE provides for non-VZW locations in scope
+# uses pd.concat https://pandas.pydata.org/pandas-docs/stable/user_guide/merging.html
+frames = [f_merged_ops, f_vzb]
+concat_ops = pd.concat(frames)
+frames_sp = [f_merged, f_vzb]
+concat_sp = pd.concat(frames_sp)
 
+# creating 2 new files, the PSPS_Main for Gennie, and a version of it with eNB/gNB for SP
 # Format the PSPS_MAIN sheet for Ops
 # establish the xlsxwriter functionality, defining "writer" as the variable for the workbook filename
 writer = pd.ExcelWriter('PSPS_MAIN.xlsx', engine='xlsxwriter')
 # Create the merged sheet and output to the file name based on the writer variable
-f_merged_ops.to_excel(writer, index=False, sheet_name='PSPS_MAIN',columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME', 'POWER_COMPANY'])
+#f_merged_ops.to_excel(writer, index=False, sheet_name='PSPS_MAIN',columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME', 'POWER_COMPANY'])
+concat_ops.to_excel(writer, index=False, sheet_name='PSPS_MAIN',columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME', 'POWER_COMPANY'])
 
 # Establish the workbook variable
 workbook = writer.book
@@ -75,7 +72,7 @@ writer.save()
 # establish the xlsxwriter functionality, defining "writer" as the variable for the workbook filename
 writer_sp = pd.ExcelWriter('PSPS_MAIN_SP.xlsx', engine='xlsxwriter')
 # Create the merged sheet and output to the file name based on the writer variable
-f_merged.to_excel(writer_sp, index = False, sheet_name='PSPS_MAIN_SP', columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME', 'POWER_COMPANY', 'eNodeB', 'GNODEB'])
+concat_sp.to_excel(writer_sp, index = False, sheet_name='PSPS_MAIN_SP', columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME', 'POWER_COMPANY', 'eNodeB', 'GNODEB'])
 
 # Establish the workbook variable
 workbook_sp = writer_sp.book
@@ -103,8 +100,3 @@ worksheet_sp.freeze_panes(1, 0)
 worksheet_sp.autofilter('A1:U9999')
 # Save the sheet
 writer_sp.save()
-
-# Still working on finding a way to replace 0's in certain columns with No, and 1's with Yes
-# df = pd.read_excel("PSPS_MAIN.xlsx")
-# df.loc[df["REMOTE_MONITORING"] == 0, "REMOTE_MONITORING"] = "No"
-# df.to_excel("PSPS_MAIN_RM.xlsx", index = False, sheet_name='PSPS_MAIN_SP', columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME', 'POWER_COMPANY', 'eNodeB', 'GNODEB'])
